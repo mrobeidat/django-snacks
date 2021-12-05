9b073456c415dea8fb21771df96d0f541af4424b
Yousef Obeidat <85868033+mrobeidat@users.noreply.github.com>
2021-12-05 16:19:23 +0200

    add test code

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,4 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 
+class django_snacks_Test(SimpleTestCase):
+
+    def test_home_status(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_status(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_about_template(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
